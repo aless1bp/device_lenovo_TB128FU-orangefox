27298b95b2a2e8907acfae4bd4f15f0378203294
Alexis <alexis@debian. local>
2025-12-05 10:06:13 -0500

    Disable OF_ADVANCED_SECURITY - only prompt password when set

--- fox_TB128FU.mk
+++ fox_TB128FU.mk
@@ -55,7 +55,7 @@ FOX_VIRTUAL_AB_DEVICE := 1
 # Maximum permissible splash image size (in kilobytes); do *NOT* increase
 OF_SPLASH_MAX_SIZE := 130
 
-OF_ADVANCED_SECURITY := 1
+OF_ADVANCED_SECURITY := 0
 
 # Custom pic for maintainer about section info
 OF_MAINTAINER := aless1bp
